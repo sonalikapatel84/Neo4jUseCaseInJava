d70de9cb6330e2d120c0cda33c6195ee5c9336c1
sonalikapatel84 <36246859+sonalikapatel84@users.noreply.github.com>
2023-12-13 14:16:37 +1100

    Initial commit for the code for practiccality project. This project will use Neo4j and java

---
+++ src/main/java/com/sonadcoder/neo4jusecase/Neo4jusecaseApplication.java
@@ -0,0 +1,13 @@
+package com.sonadcoder.neo4jusecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jusecaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jusecaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonadcoder/neo4jusecase/Neo4jusecaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonadcoder.neo4jusecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jusecaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
